5a67a00f40ba801f9a003cccb4b6685a131e71b4
dodo150243 <tirawat330@gmail.com>
2021-09-28 09:02:56 +0700

    update db

--- alumni_project/app/Config/Database.php
+++ alumni_project/app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'alumni_project_new',
+        'database' => 'alumni_project',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
