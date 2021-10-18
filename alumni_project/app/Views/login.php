6ae3032a7bed41f3ea1b5485e5e5534be9f550a0
ntpoom9 <zerpoom01@gmail.com>
2021-10-18 20:00:34 +0700

    add logo in tab

--- alumni_project/app/Views/login.php
+++ alumni_project/app/Views/login.php
@@ -22,8 +22,10 @@
     <!-- Icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-
-        <link rel="stylesheet" href="/css/login.css"> 
+    <!-- icon tab  -->
+    <link rel="shortcut icon" type="image/x-icon" href="/img/logo_isac.png" />
+    <!-- css  -->
+    <link rel="stylesheet" href="/css/login.css">
 
 </head>
 
